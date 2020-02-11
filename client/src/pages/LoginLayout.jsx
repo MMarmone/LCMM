6f0f42b098790b75736dc4c3342c6f23d2d09f1e
FilipeCosta06 <costafilipe06@hotmail.fr>
2020-02-11 20:31:27 +0100

    UI fix
    padding top sur les formulaires login/reister

--- client/src/pages/LoginLayout.jsx
+++ client/src/pages/LoginLayout.jsx
@@ -8,7 +8,7 @@ export default function LoginLayout (props) {
 
     return (
         <ResponsiveContainer inverted={props.inverted}>
-            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            <Grid textAlign='center' style={{ paddingTop: '5vh' }} verticalAlign='middle'>
                 <Grid.Column style={{ maxWidth: 450 }}>
                     <Header as='h2' color='orange' textAlign='center'>
                         <Image src={process.env.PUBLIC_URL + '/logo192.png'} /> Log-in to your account
