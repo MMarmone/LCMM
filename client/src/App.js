7d87508946e4d87b2f03b27345882ccfed6405c4
Filipe <costafilipe06@hotmail.fr>
2020-02-12 12:36:23 +0100

    bidouillage qui ne marche pas
    j'expérimente avec useContext (pour ne pas avoir à se passer 1 milliards d'états de haut en bas)
    
    Au pire, je mettrai juste le token dans les cookies/localStorage mais ça va pas trigger un redraw :/

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 import {Route, Switch} from "react-router-dom";
 import HomeLayout from "./pages/HomeLayout";
@@ -6,29 +6,76 @@ import LoginLayout from "./pages/LoginLayout";
 import RegisterLayout from "./pages/RegisterLayout";
 import PageNotFoundLayout from "./pages/PageNotFountLayout";
 
+const initialState = {
+  isLoggedIn: null,
+  userToken: null
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'login':
+      return {
+        isLoggedIn: true,
+        userToken: action.userToken,
+        ...initialState
+      };
+
+    case 'logout':
+      return {
+        isLoggedIn: false,
+        userToken:null,
+        ...initialState
+      };
+
+    default:
+      throw new Error("Unknown dispatch action called (" + action.type + ")");
+  }
+}
+
+/*
+function AppContextProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return
+} // */
+
+export const AppDispatch = React.createContext(null);
+
 function App() {
-    // todo darkmode configurable
-    const [inverted, setInverted] = useState(true);
+  // todo darkmode configurable
+  //  via https://reactjs.org/docs/hooks-reference.html#usecontext
+  //  peut également servir pour l'état isLoggedIn nan ? Ou alors on stock le token en cookie/localstorage
 
-    return (
-        <Switch>
-            <Route exact path="/">
-                <HomeLayout inverted={inverted} />
-            </Route>
+  const [inverted, setInverted] = useState(true);
+  // const [isUserLoggedIn, setIsUserLoggedIn] = useState(false); // initialiser avec la valeur du cookie / locastorage
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-            <Route path="/login">
-                <LoginLayout inverted={inverted} />
-            </Route>
 
-            <Route path="/register">
-                <RegisterLayout inverted={inverted} />
-            </Route>
 
-            <Route path="*">
-                <PageNotFoundLayout inverted={inverted} />
-            </Route>
-        </Switch>
-    );
+  return (
+    <AppDispatch.Provider value={dispatch}>
+      <p>isLoggedIn = {state.isLoggedIn ? "true":"false"}</p>
+      <p>initialState = {JSON.stringify(initialState)}</p>
+      <br />
+      <Switch>
+        <Route exact path="/">
+          <HomeLayout isLoggedIn={state.isLoggedIn} inverted={inverted} />
+        </Route>
+
+        <Route path="/login">
+          <LoginLayout inverted={inverted} />
+        </Route>
+
+        <Route path="/register">
+          <RegisterLayout inverted={inverted} />
+        </Route>
+
+        <Route path="*">
+          <PageNotFoundLayout inverted={inverted} />
+        </Route>
+      </Switch>
+    </AppDispatch.Provider>
+  );
 }
 
 export default App;
