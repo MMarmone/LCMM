0acfc7bd19675651dad6e4423bbca2ac4ad88c66
LotfiYacine <dtriamort@gmail.com>
2020-02-25 16:04:52 +0100

    ajout route + layout my cart

---
+++ client/src/components/Cart/MyCart.jsx
@@ -0,0 +1,11 @@
+import React, {useContext} from "react";
+import {store} from "../StateProvider/StateProvider";
+
+export default function MyCart() {
+    const {state} = useContext(store);
+    return (
+        <div>
+
+        </div>
+    )
+}
